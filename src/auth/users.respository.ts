8343034b8808ebf81f4912e6c99cc25950a9d0e9
Mahnaz Fakhr <mahfakhr2009@gmail.com>
2023-02-11 18:54:39 +0330

    develop: added auth and signup with hashed pass using bcrypt

--- src/auth/users.respository.ts
+++ src/auth/users.respository.ts
@@ -1,5 +1,26 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { User } from './user.entity';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import * as bcrypt from 'bcryptjs';
 
 @EntityRepository(User)
-export class UsersRespository extends Repository<User> {}
+export class UsersRespository extends Repository<User> {
+  async createUser(authCredentialDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialDto;
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const user = this.create({ username, password: hashedPassword });
+    try {
+      await this.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Username already exists!');
+      }
+      throw new InternalServerErrorException();
+    }
+  }
+}
