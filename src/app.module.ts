781083998426bf76b3d54c3fe8af88b1cbd0e0ae
Mahnaz Fakhr <mahfakhr2009@gmail.com>
2023-03-20 19:23:52 +0330

    feat: added schema validation for app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import * as process from 'process';
+import { configValidationSchema } from "./config.schema";
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: [`.env.stage.${process.env.STAGE}`],
+      validationSchema:configValidationSchema
     }),
     TasksModule,
     TypeOrmModule.forRootAsync({
